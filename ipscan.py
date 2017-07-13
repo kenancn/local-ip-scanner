84cc5eb23c142865e8737eb6b0b133d2d6bead00
Kenan <ibisc27@gmail.com>
2017-07-13 10:26:50 +0300

    Edit

---
+++ ipscan.py
@@ -0,0 +1,31 @@
+# typo error in import
+import subprocess
+ip = raw_input("Lutfen ip degerini giriniz:(orn: 192.168.1)")
+aralik = raw_input("Lutfen araligi belirleyiniz:(orn:255)")
+lstaktif=[]
+lstpasif=[]
+for ping in range(1,int(aralik)):
+    address = ip + "." + str(ping)
+    print ip
+    res = subprocess.call(['ping', '-w' , '500' , '-c', '2', address])
+    clc = subprocess.call(['clear'])
+    if res == 0:
+        lstaktif.append(address)
+    elif res == 2:
+        print "no response from", address
+    else:
+        lstpasif.append(address)
+
+print "*************************************************************************"
+for adress in range(len(lstaktif)):
+    print lstaktif[adress] , ": Aktif"
+print "-------------------------------------------------------------------------"
+for adress in range(len(lstpasif)):
+    print lstpasif[adress] , ": Pasif"
+
+
+
+
+
+
+
